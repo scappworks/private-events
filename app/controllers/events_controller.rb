00f9e522d1e4e8bb183860cf047283185a64bb87
scott <scappworks@gmail.com>
2021-01-30 02:57:34 -0500

    add events index view. add event show page

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -3,27 +3,31 @@ class EventsController < ApplicationController
         @events = Event.all
     end
 
+    def show
+        @event = Event.find(params[:id])
+    end
+
     def new
         @event = Event.new
     end
 
     def create
-        @event = Event.build(event_params)
+        current_user = User.find_by(id: session[:id])
+        @event = current_user.events.build(event_params)
 
         if @event.save
             flash.notice = "Event created!"
+            
         else
             flash.alert = "Event not created!"
         end
-    end
 
-    def show
-        @event = Event.find(params[:event_name])
+        redirect_to event_path(@event)
     end
 
     private
 
     def event_params
-        params.require(:event).permit(:event_name, :event_date, :user_id)
+        params.require(:event).permit(:event_name, :event_description, :event_date, :user_id)
     end
 end
