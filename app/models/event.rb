cf790a441a4b4800753631a37e94e366c2773e9a
scott <scappworks@gmail.com>
2021-01-31 01:48:57 -0500

    add scoped class methods for 'past' and 'upcoming' events to event model. modify events index view and user show view to use new event class methods

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,6 @@
 class Event < ApplicationRecord
     belongs_to :creator, class_name: "User", foreign_key: "user_id"
     has_many :attendees, through: :enrollments, source: :user
+    scope :past, -> { where('event_date < ?', Date.today) }
+    scope :upcoming, -> { where('event_date >= ?', Date.today) }
 end
