c6189da71e979928ea0bf8742a65c02523f1ccec
scott <scappworks@gmail.com>
2021-02-03 01:58:32 -0500

    add current_user method to application controller to stop users from creating posts when they are not signed in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,6 @@
 class ApplicationController < ActionController::Base
+    def current_user
+        @current_user ||= User.find(session[:id]) if session[:id]
+    end
+    helper_method :current_user
 end
