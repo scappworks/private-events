22ac8197d49152b28400495063b48e592d90edd0
scott <scappworks@gmail.com>
2021-02-10 13:44:08 -0500

    add destroy to enrollment controller. add uninvite form to event view. create route for uninvite path

--- app/controllers/enrollments_controller.rb
+++ app/controllers/enrollments_controller.rb
@@ -4,11 +4,26 @@ class EnrollmentsController < ApplicationController
         enrollment = Enrollment.new(event_id: event.id, user_id: params[:user_id])
 
         if enrollment.save
-            flash[:notice] = "Enrollment created"
+            flash[:notice] = user.username + " was invited!"
         else
-            flash[:alert] = "Enrollment not saved"
+            flash[:alert] = "Invitation not sent"
         end
 
         redirect_to event_path(event.id)
+    end
+
+    def destroy
+        event = Event.find(params[:event_id])
+        enrollment = Enrollment.find_by(event_id: params[:event_id], user_id: params[:user_id])
+        user = User.find(enrollment.user_id)
+        enrollment.delete
+
+        if enrollment.destroy
+            flash[:notice] = user.username + " was uninvited"
+        else
+            flash[:alert] = "Invitation not canceled"
+        end
+
+        redirect_to event_path(current_user.events.find(event.id))
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   post "/login", to: "sessions#create"
   delete "/logout", to: "sessions#destroy"
   post "/invite", to: "enrollments#create"
+  delete "/uninvite", to: "enrollments#destroy"
 
   root "events#index"
 end
