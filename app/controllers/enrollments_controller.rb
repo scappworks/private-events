c5ea6425c046fa1a2dca963191f43f2479b1b99e
scott <scappworks@gmail.com>
2021-02-11 03:42:08 -0500

    change enrollments controller to allow all users to send invites to other uses on any event. change events show page to accept username input instead of user id

--- app/controllers/enrollments_controller.rb
+++ app/controllers/enrollments_controller.rb
@@ -1,7 +1,8 @@
 class EnrollmentsController < ApplicationController
     def create
         event = Event.find(params[:event_id])
-        enrollment = Enrollment.new(event_id: event.id, user_id: params[:user_id])
+        user = User.find_by(username: params[:username])
+        enrollment = Enrollment.new(event_id: event.id, user_id: user.id)
 
         if enrollment.save
             flash[:notice] = user.username + " was invited!"
@@ -14,8 +15,8 @@ class EnrollmentsController < ApplicationController
 
     def destroy
         event = Event.find(params[:event_id])
-        enrollment = Enrollment.find_by(event_id: params[:event_id], user_id: params[:user_id])
-        user = User.find(enrollment.user_id)
+        user = User.find_by(username: params[:username])
+        enrollment = Enrollment.find_by(event_id: event.id, user_id: user.id)
         enrollment.delete
 
         if enrollment.destroy
@@ -24,6 +25,6 @@ class EnrollmentsController < ApplicationController
             flash[:alert] = "Invitation not canceled"
         end
 
-        redirect_to event_path(current_user.events.find(event.id))
+        redirect_to event_path(event.id)
     end
 end
