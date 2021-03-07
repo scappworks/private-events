0dc4d260e174884133004b23084509cae40bd12c
scott <scappworks@gmail.com>
2021-03-07 01:54:17 -0500

    change gemfile and database.yml to use postgresql instead of sqlite

--- db/schema.rb
+++ db/schema.rb
@@ -12,9 +12,12 @@
 
 ActiveRecord::Schema.define(version: 2021_01_31_052836) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "enrollments", force: :cascade do |t|
-    t.integer "user_id"
-    t.integer "event_id"
+    t.bigint "user_id"
+    t.bigint "event_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["event_id"], name: "index_enrollments_on_event_id"
